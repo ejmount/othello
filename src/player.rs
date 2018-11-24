5d0e290e349cee280422cfef64b412c749db4916
Ewan Mount <ejmount@users.noreply.github.com>
2018-11-24 18:08:21 +0000

    Adding Player lifetime

--- src/player.rs
+++ src/player.rs
@@ -1,7 +1,7 @@
 use crate::board::Board;
 use crate::board::Move;
 
-pub trait Player {
+pub trait Player: 'static {
     fn get_move(&mut self, b: &Board, availiable: &mut Iterator<Item = Move>) -> Option<Move>;
 }
 
@@ -9,9 +9,6 @@ pub struct BasicPlayer {}
 
 impl Player for BasicPlayer {
     fn get_move(&mut self, _b: &Board, availiable: &mut Iterator<Item = Move>) -> Option<Move> {
-        let m = availiable.next();
-        /*println!("Applying {:?} to...", m);
-        println!("{}", b);*/
-        return m;
+        availiable.next()
     }
 }
